6dd0212925bd0627d3dec66416a17710275b274d
josemibnf <josemi.bnf@gmail.com>
2021-06-15 13:19:28 +0200

    ..

--- script.py
+++ script.py
@@ -1,13 +1,13 @@
 import grpc, gateway_pb2, gateway_pb2_grpc, api_pb2, api_pb2_grpc
 from time import sleep
 
-SORTER = "7a7d365af4a395f31e4402f0aff803af962c5f4226b592cebaa0fe76a709aab4"
+SORTER = "16cf922a140802ca86d6a96614883e7e8d52eb9d4334dfe667364629eb576e7e"
 RANDOM = 'e92d52639d2f582d4c9f148ae776abd14ebad4c261d06672e27f317008641200'
 FRONTIER = 'e740d93e8e9cbedb917c00ccd9887d934f881ab3812867a596019116ebbb31db'
 WALK = '9142c5317ea881d20c5553fbf0403950db5bebfea1e0ea3cd7306febe5b78f98'
 WALL = '436b35c87727d8856cdf77fe176a5dbc715bbd8a78dffccbd057ba4f3c15e060'
 
-GATEWAY = '192.168.1.172'
+GATEWAY = '192.168.1.143'
 
 def generator(hash):
     transport = gateway_pb2.ServiceTransport()
@@ -32,25 +32,11 @@ c_stub = api_pb2_grpc.SolverStub(
         )
     )
 
+print('Tenemos clasificador. ', c_stub)
+
 sleep(10) # Espera a que el servidor se levante.
 
-# Inicia el entrenamiento.
-c_stub.StartTrain(api_pb2.Empty())
-
-# Añade solvers.
-for s in [FRONTIER, WALL, WALK]:
-    solver = api_pb2.ipss__pb2.Service()
-    solver.ParseFromString(open('__registry__/'+s+'.service', 'rb').read())
-    c_stub.UploadSolver(solver)
-
-print('Wait to train the model ...')
-for i in range(10): 
-    # Realiza regresion 20 veces, el tiempo por defecto entre cada una son 900 segundos
-    print('TEMPO ', i)
-    sleep(900)
-
-
-# While classifier is training get random cnf
+# Get random cnf
 random_cnf_service = g_stub.StartService(generator(
     hash=RANDOM
 ))
@@ -62,32 +48,49 @@ r_stub = api_pb2_grpc.RandomStub(
         )
     )
 
-sleep(5) # Espera a que el servidor se levante.
+print('Tenemos random. ', r_stub)
 
-cnf = r_stub.RandomCnf(api_pb2.Empty())
+print('Iniciando entrenamiento...')
+# Inicia el entrenamiento.
+c_stub.StartTrain(api_pb2.Empty())
 
-# Stop Random cnf service.
-g_stub.StopService(random_cnf_service.token)
-print('Our cnf for the test is ', cnf)
+print('Subiendo solvers al clasificador.')
+# Añade solvers.
+for s in [FRONTIER, WALL, WALK]:
+    print('     ', s)
+    solver = api_pb2.ipss__pb2.Service()
+    solver.ParseFromString(open('__registry__/'+s+'.service', 'rb').read())
+    c_stub.UploadSolver(solver)
 
 
+print('Wait to train the model ...')
+for i in range(5): 
+    for j in range(10):
+        print(' time ', i, j)
+        sleep(200)
+    
+    cnf = r_stub.RandomCnf(api_pb2.Empty())
+    # Comprueba si sabe generar una interpretacion (sin tener ni idea de que tal
+    # ha hecho la seleccion del solver.)
+    print('\n ---- ', i)
+    print('SOLVING CNF ...')
+    interpretation = c_stub.Solve(cnf)
+    print('OKAY THE INTERPRETATION WAS ', interpretation)
 
 
 # Termina el entrenamiento.
 c_stub.StopTrain(api_pb2.Empty())
 
+cnf = r_stub.RandomCnf(api_pb2.Empty())
 # Comprueba si sabe generar una interpretacion (sin tener ni idea de que tal
 # ha hecho la seleccion del solver.)
 
 print('SOLVING CNF ...')
-
 interpretation = c_stub.Solve(cnf)
-
-print('OKAY THE INTERPRETATION WAS ', interpretation)
-
-input("Press Enter to continue...")
-
-
+print('THE FINAL INTERPRETATION WAS ', interpretation)
 
 # Stop the classifier.
 g_stub.StopService(classifier.token)
+
+# Stop Random cnf service.
+g_stub.StopService(random_cnf_service.token)
--- sorter_on_local.py
+++ sorter_on_local.py
@@ -6,7 +6,7 @@ FRONTIER = 'e740d93e8e9cbedb917c00ccd9887d934f881ab3812867a596019116ebbb31db'
 WALK = '9142c5317ea881d20c5553fbf0403950db5bebfea1e0ea3cd7306febe5b78f98'
 WALL = '436b35c87727d8856cdf77fe176a5dbc715bbd8a78dffccbd057ba4f3c15e060'
 
-GATEWAY = '192.168.1.172'
+GATEWAY = '192.168.1.143'
 
 def generator(hash):
     transport = gateway_pb2.ServiceTransport()
@@ -23,20 +23,8 @@ c_stub = api_pb2_grpc.SolverStub(
 # Inicia el entrenamiento.
 c_stub.StartTrain(api_pb2.Empty())
 
-# Añade solvers.
-for s in [FRONTIER, WALL, WALK]:
-    solver = api_pb2.ipss__pb2.Service()
-    solver.ParseFromString(open('__registry__/'+s+'.service', 'rb').read())
-    c_stub.UploadSolver(solver)
 
-print('Wait to train the model ...')
-for i in range(10): 
-    # Realiza regresion 20 veces, el tiempo por defecto entre cada una son 900 segundos
-    print('TEMPO ', i)
-    sleep(900)
-
-print('Vamos a generar un cnf.')
-
+# Obteniendo servicio de cnf's random.
 g_stub = gateway_pb2_grpc.GatewayStub(
     grpc.insecure_channel(GATEWAY+':8080')
     )
@@ -55,11 +43,26 @@ r_stub = api_pb2_grpc.RandomStub(
         )
     )
 
-sleep(5) # Espera a que el servidor se levante.
+# Añade solvers.
+for s in [FRONTIER, WALL, WALK]:
+    solver = api_pb2.ipss__pb2.Service()
+    solver.ParseFromString(open('__registry__/'+s+'.service', 'rb').read())
+    c_stub.UploadSolver(solver)
 
-cnf = r_stub.RandomCnf(api_pb2.Empty())
+print('Wait to train the model ...')
+for i in range(10): 
+    sleep(80)
+    
+    cnf = r_stub.RandomCnf(api_pb2.Empty())
+    # Comprueba si sabe generar una interpretacion (sin tener ni idea de que tal
+    # ha hecho la seleccion del solver.)
+    print('\n ---- ', i)
+    print('SOLVING CNF ...')
+    interpretation = c_stub.Solve(cnf)
+    print('OKAY THE INTERPRETATION WAS ', interpretation)
 
-print('Our cnf for the test is ', cnf)
+print('Vamos a generar un cnf.')
+
 
 # Stop Random cnf service.
 g_stub.StopService(random_cnf_service.token)
