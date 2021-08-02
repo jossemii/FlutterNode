86f61db554fc1e82f0e45e5d6efcf23f9b4ad229
josemibnf <josemi.bnf@gmail.com>
2021-08-02 13:38:55 +0200

    .

--- script.py
+++ script.py
@@ -1,16 +1,20 @@
 import grpc, gateway_pb2, gateway_pb2_grpc, api_pb2, api_pb2_grpc, threading, json, sys
-from six import with_metaclass
 from time import sleep, time
 
-SORTER = "e15dc1dc49db2c01564e20dbaab95f085f03c9259ecc05c0e5a3336f4ef5378c"
-RANDOM = 'e92d52639d2f582d4c9f148ae776abd14ebad4c261d06672e27f317008641200'
-FRONTIER = 'e740d93e8e9cbedb917c00ccd9887d934f881ab3812867a596019116ebbb31db'
-WALK = '9142c5317ea881d20c5553fbf0403950db5bebfea1e0ea3cd7306febe5b78f98'
-WALL = '436b35c87727d8856cdf77fe176a5dbc715bbd8a78dffccbd057ba4f3c15e060'
+if input('Clean json data? (y/n)') == 'y':
+    print('\nClearing data ...')
+    with open('script_data.json', 'w') as file:
+        json.dump("", file)
 
-GATEWAY = '192.168.1.143'
+SORTER = "6565343d16cc49f998f0313d787a43a1ec42324fb3f2103f661672a2745f0ed1"
+RANDOM = '09fb4947e59b569eb3dd5726a4e52d7244dc562852559b2efda2a5234f6ff446'
+FRONTIER = 'e0613ac5773a46f22d119b0c4fc1f6047f3f90c4cd677edee89bcab76960b208'
+WALK = 'ddd36870083b58df28db54391552143e231f1b5d93937efd3cb1dae179dacae6'
+WALL = '9d79082de68b72b8c2fb7bcc3a229374cca11794f1b5d2cb5f47a22b9a1c820b'
 
-TRAIN = True if len(sys.argv)>1 and sys.argv[1] == 'train' else False
+WHISKY = '192.168.1.114'
+MOJITO = '192.168.1.143'
+GATEWAY = MOJITO
 
 def generator(hash):
     transport = gateway_pb2.ServiceTransport()
@@ -22,11 +26,15 @@ g_stub = gateway_pb2_grpc.GatewayStub(
     grpc.insecure_channel(GATEWAY+':8080')
     )
 
-if open('script_data.json', 'r').read() == '':
+print(json.load(open('script_data.json', 'r')))
+if type(json.load(open('script_data.json', 'r'))) != dict:
+
+    print('Get new services....')
     # Get a classifier.
     classifier = g_stub.StartService(generator(
         hash=SORTER
     ))
+    print(classifier)
 
     uri=classifier.instance.uri_slot[0].uri[0]
     c_uri = uri.ip +':'+ str(uri.port)
@@ -36,13 +44,12 @@ if open('script_data.json', 'r').read() == '':
 
     print('Tenemos clasificador. ', c_stub)
 
-    sleep(10) # Espera a que el servidor se levante.
-
     # Get random cnf
     random_cnf_service = g_stub.StartService(generator(
         hash=RANDOM
     ))
 
+    print(random_cnf_service)
     uri=random_cnf_service.instance.uri_slot[0].uri[0]
     r_uri = uri.ip +':'+ str(uri.port)
     r_stub = api_pb2_grpc.RandomStub(
@@ -62,6 +69,7 @@ else:
     print('Getting from json file.')
     with open('script_data.json', 'r') as file:
         data = json.load(file)
+        print(data)
         c_stub = api_pb2_grpc.SolverStub(
             grpc.insecure_channel(data['sorter'])
         )
@@ -69,12 +77,12 @@ else:
             grpc.insecure_channel(data['random'])
         )
 
-if TRAIN:
+sleep(10) # Espera a que el servidor se levante.
+
+if input("\nGo to train? (y/n)")=='y':
     print('Iniciando entrenamiento...')
     # Inicia el entrenamiento.
     c_stub.StartTrain(api_pb2.Empty())
-
-
 
     print('Subiendo solvers al clasificador.')
     # Añade solvers.
@@ -118,7 +126,7 @@ def logs(c_stub):
     for file in c_stub.StreamLogs(api_pb2.Empty()):
         print('\n\nNEW FILE.', file.file)
 
-for i in range(20):
+for i in range(3):
     sleep(10)
     threads = []
     for j in range(10 if i%2==0 else 4):
@@ -152,4 +160,5 @@ g_stub.StopService(random_cnf_service.token)
 print('All good?')
 
 with open('script_data.json', 'w') as file:
-    json.dump('', file)
+    print('Clearing data ...')
+    json.dump("", file)
