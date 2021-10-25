2b605025ff7ad70c3114bd6573b16ddfbeefaf6e
josemibnf <josemi.bnf@gmail.com>
2021-10-25 18:00:45 +0200

    asd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 RANDOM = 'b81522c517c9d01dcf8c7ded181f5ecbeac6bb974adb38e90fd4cec82501b86a'
-FRONTIER = '88ebf38754178d0258e975e72600ceae42a36bb4bbd50953d88150ec7aa5e577'
-WALL = ''
+FRONTIER = '257c242d1b2eb5a9b83d8c7a72fbe9f6f41d24e87987c64e2591b6f7dc0dfe84'
+WALL = 'fc85bf87ab12e0651eaf097ce9c0c8a984a0cd11015f7f6194ddd51c3d7dcf7f'
 WALK = ''
 LISIADO_UNDER = ''
 LISIADO_OVER = ''
@@ -12,9 +12,6 @@ MOJITO = '192.168.1.144'
 TEQUILA = '192.168.1.63'
 GATEWAY = MOJITO
 
-
-CHUNK_SIZE = 1024 * 1024  # 1MB
-import gateway_pb2
 def generator(hash: str):
     yield gateway_pb2.ServiceTransport(
         hash = gateway_pb2.celaut__pb2.Any.Metadata.HashTag.Hash(
@@ -24,8 +21,33 @@ def generator(hash: str):
         config = gateway_pb2.celaut__pb2.Configuration()
     )
 
+CHUNK_SIZE = 1024 * 1024  # 1MB
+import os
+import gateway_pb2
+from random import randint
 from typing import Generator
+from threading import Condition
 
+class Signal():
+    # The parser use change() when reads a signal on the buffer.
+    # The serializer use wait() for stop to send the buffer if it've to do it.
+    # It's thread safe because the open var is only used by one thread (the parser) with the change method.
+    def __init__(self, exist: bool = True) -> None:
+        self.exist = exist
+        if exist: self.open = True
+        if exist: self.condition = Condition()
+    
+    def change(self):
+        if self.exist:
+            if self.open:
+                self.open = False  # Stop the input buffer.
+            else:
+                self.condition.notify_all()
+                self.open = True  # Continue the input buffer.
+    
+    def wait(self):
+        if self.exist and not self.open:
+            self.condition.wait()
 
 def get_file_chunks(filename) -> Generator[gateway_pb2.Buffer, None, None]:
     with open(filename, 'rb') as f:
@@ -41,14 +63,18 @@ def save_chunks_to_file(chunks: gateway_pb2.Buffer, filename):
         for buffer in chunks:
             f.write(buffer.chunk)
 
-def parse_from_buffer(request_iterator, message_field = None):
+def parse_from_buffer(request_iterator, message_field = None, signal = Signal(exist=False)):
     while True:
         all_buffer = bytes('', encoding='utf-8')
         while True:
             buffer = next(request_iterator)
             if buffer.HasField('separator'):
                 break
-            all_buffer += buffer.chunk
+            if buffer.HasField('signal'):
+                signal.change()
+                continue
+            if buffer.HasField('chunk'):
+                all_buffer += buffer.chunk
         if message_field: 
             message = message_field()
             message.ParseFromString(
@@ -58,26 +84,47 @@ def parse_from_buffer(request_iterator, message_field = None):
         else:
             yield all_buffer # Clean buffer index bytes.
 
-def serialize_to_buffer(message_iterator):
+def serialize_to_buffer(message_iterator, signal = Signal(exist=False)):
     if not hasattr(message_iterator, '__iter__'): message_iterator=[message_iterator]
     for message in message_iterator:
-        byte_list = list(message.SerializeToString())
-        for chunk in [byte_list[i:i + CHUNK_SIZE] for i in range(0, len(byte_list), CHUNK_SIZE)]:
-            b = gateway_pb2.Buffer(
-                            chunk = bytes(chunk)
-                        )
-            yield b
+        message_bytes = message.SerializeToString()
+        if len(message_bytes) < CHUNK_SIZE:
+            signal.wait()
+            yield gateway_pb2.Buffer(
+                chunk = bytes(message_bytes)
+            )
+        else:
+            try:
+                byte_list = list(message_bytes)
+                for chunk in [byte_list[i:i + CHUNK_SIZE] for i in range(0, len(byte_list), CHUNK_SIZE)]:
+                    signal.wait()
+                    yield gateway_pb2.Buffer(
+                                    chunk = bytes(chunk)
+                                )
+            except: # INEFICIENT.
+                file =  os.path.abspath(os.curdir) + '/__hycache__/' + str(len(message_bytes)) + ':' + str(randint(1,999))
+                open(file, 'wb').write(message_bytes)
+                try:
+                    for b in get_file_chunks(file): 
+                        signal.wait()
+                        yield b
+                finally:
+                    os.remove(file)
+
         yield gateway_pb2.Buffer(
             separator = bytes('', encoding='utf-8')
         )
 
 def client_grpc(method, output_field = None, input=None, timeout=None):
+    signal = Signal()
     for b in parse_from_buffer(
         request_iterator = method(
                             serialize_to_buffer(
-                                input if input else ''
+                                input if input else '',
+                                signal = signal
                             ),
                             timeout = timeout
                         ),
-        message_field = output_field
+        message_field = output_field,
+        signal = signal
     ): yield b
--- test.py
+++ test.py
@@ -1,6 +1,26 @@
+from time import sleep
 import grpc, gateway_pb2, gateway_pb2_grpc, api_pb2_grpc, api_pb2, celaut_pb2
 
-from main import GATEWAY, FRONTIER, client_grpc, generator
+from main import GATEWAY, WALL, RANDOM, client_grpc
+
+def service_extended(hash):
+    any = api_pb2.celaut__pb2.Any()
+    any.ParseFromString(open('__registry__/'+hash, 'rb').read())
+    config = True
+    transport = gateway_pb2.ServiceTransport()
+    for hash in any.metadata.hashtag.hash:
+        transport.hash.CopyFrom(hash)
+        if config:  # Solo hace falta enviar la configuracion en el primer paquete.
+            transport.config.CopyFrom(celaut_pb2.Configuration())
+            config = False
+        yield transport
+    transport.ClearField('hash')
+    if config: transport.config.CopyFrom(celaut_pb2.Configuration())
+
+    transport.service.service.ParseFromString(any.value)
+    transport.service.meta.CopyFrom(any.metadata)
+    yield transport
+
 
 def get_grpc_uri(instance: celaut_pb2.Instance) -> celaut_pb2.Instance.Uri:
     for slot in instance.api.slot:
@@ -16,37 +36,57 @@ g_stub = gateway_pb2_grpc.GatewayStub(
     grpc.insecure_channel(GATEWAY + ':8080'),
 )
 
-print('Get new services....')
 
-c_stub = api_pb2_grpc.SolverStub(
-    grpc.insecure_channel('localhost:8080')
-    )
-
-print('Tenemos clasificador. ', c_stub)
-
-# Get random cnf
-service = next(client_grpc(
+# Get solver cnf
+print('\n\nGet new services....')
+solver = next(client_grpc(
     method = g_stub.StartService,
     output_field=gateway_pb2.Instance,
-    input=generator(hash=FRONTIER)
+    input=service_extended(hash=WALL)
 ))
 
-uri = get_grpc_uri(service.instance)
+uri = get_grpc_uri(solver.instance)
+solver_stub = api_pb2_grpc.SolverStub(
+    grpc.insecure_channel(
+        uri.ip + ':' + str(uri.port)
+    )
+)
+solver_token = solver.token
+
+print(' SOLVER SERVICE -> ', uri)
+
+
+
+# Get random cnf
+print('\n\nGet new services....')
+random = next(client_grpc(
+    method = g_stub.StartService,
+    output_field=gateway_pb2.Instance,
+    input=service_extended(hash=RANDOM)
+))
+
+uri = get_grpc_uri(random.instance)
 random_stub = api_pb2_grpc.RandomStub(
     grpc.insecure_channel(
         uri.ip + ':' + str(uri.port)
     )
 )
-random_token = service.token
+random_token = random.token
 
-print('RANDOM SERVICE -> ', uri, random_token)
-
-"""
+print(' RANDOM SERVICE -> ', uri)
+sleep(1)
 cnf = next(client_grpc(
-            method = random_stub.RandomCnf,
-            input = api_pb2.Empty(),
-            output_field = api_pb2.Cnf
+    method=random_stub.RandomCnf,
+    input = api_pb2.Empty(),
+    output_field=api_pb2.Cnf
 ))
 
-print('Cnf -> ', cnf)
-"""
+print('CNF -> ', cnf)
+
+interpretation  = next(client_grpc(
+    method=solver_stub.Solve,
+    input=cnf,
+    output_field=api_pb2.Interpretation
+))
+
+print('Interpretation -> ', interpretation)
